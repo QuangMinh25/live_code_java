52c27444989067e9c120886be840f499b3c700e2
裴光明 <web.quangminh@pyv.com.vn>
2025-11-13 11:00:20 +0700

    add exam

---
+++ learnJava/Variable.java
@@ -0,0 +1,15 @@
+package learnJava;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+
+        String name = "John";
+        int age = 20;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
